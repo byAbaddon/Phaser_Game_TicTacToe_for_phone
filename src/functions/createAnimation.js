a87430bfae9b23f9f7ce2d77caaec5d2e44da592
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-11-10 00:14:43 +0200

    Add files via upload

---
+++ src/functions/createAnimation.js
@@ -0,0 +1,26 @@
+function createAnimationByArrayOfImages(currentScene = null, key = '', arrayImages = [], repeat = -1, frameRate= 10) {
+ const animation =  currentScene.anims.create({
+    key,
+    frames: arrayImages.map(imageKey => ({ key: imageKey })),
+    frameRate,
+    repeat,
+    
+ })
+  
+  return animation
+}
+
+
+function createAnimationBySpriteOfImages(currentScene = null, key = '', spriteName = null, start = 0, end = 0, repeat = -1, frameRate = 10) {
+  const spiteAnimation = currentScene.anims.create({
+    key,
+    frames: currentScene.anims.generateFrameNumbers(spriteName, { start, end }),
+    frameRate,
+    repeat,
+  })
+  return spiteAnimation
+}
+
+
+export { createAnimationByArrayOfImages, createAnimationBySpriteOfImages }
+
